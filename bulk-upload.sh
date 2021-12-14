29a72fcb9ff29348e37bfd39566a4179faffc4f5
Aleksey Polygaev <a.polygaev@bookmate.com>
2021-12-14 17:46:43 +0300

    First working version

---
+++ bulk-upload.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# Script to upload multiple images to a.bmstatic.com using image-uploader.bookmate.services
+#
+# Usage:
+# script <images_root_directory>
+#
+# Required packages:
+# curl, sed, coreutils, findutils
+
+# Default settings
+endpoint="image-uploader.bookmate.services/upload"
+app_curl=$(type -p curl)
+
+output_failed="upload.failed"
+output_success="upload.success"
+
+# Check defaults
+if [ -z "${app_curl}" ]; then
+    echo "Please install 'curl' package"
+    exit 1
+fi
+
+# Help function
+function print_help()
+{
+    echo "A script to upload images to a.bmstatic.com using image-uploader.bookmate.services"
+    echo "Usage:"
+    echo "${0} <images_root_directory>"
+}
+
+# Other functions
+function curl_upload_file()
+{
+    curl -is -X POST -H 'Content-Type: multipart/form-data' -F "data=@${image}" "${endpoint}"
+}
+
+fsize_bytes()
+{
+    stat -c '%s' "${1}"
+}
+
+fsize_print()
+{
+    bytes=$(fsize_bytes "${1}")
+
+    div=$((1024))
+    if [ ${bytes} -ge ${div} ]; then
+        echo "$((bytes / div)) KB"
+        return 0
+    fi
+
+    div=$((1024 * 1024))
+    if [ ${bytes} -ge ${div} ]; then
+        echo "$((bytes / div)) MB"
+        return 0
+    fi
+
+    div=$((1024 * 1024 * 1024))
+    if [ ${bytes} -ge ${div} ]; then
+        echo "$((bytes / div)) GB"
+        return 0
+    fi
+
+    echo "${bytes}"
+}
+
+image_url()
+{
+    # 1. Get last line in curl output
+    # 2. Get image url from ["<url here>"]
+    tail -n 1 <<< "${1}" | sed -nr 's/\[\"(.*)\"\]/\1/p'
+}
+
+http_codes_list()
+{
+    # 1. Remove CR from curl output
+    # 2. Print HTTP ret codes after 'HTTP/1.1' pattern
+    sed 's/\r//g'  <<< "${1}" | sed -nr "s/HTTP\/[0-9]\.[0-9] (.*)/\1/p"
+}
+
+# Parse command line parameters
+if [ -z "${1}" ]; then
+    print_help
+    exit 1
+fi
+
+images_root_dir="${1}"
+
+# Check command line parameters
+if [ ! -d "${1}" ]; then
+    echo "Directory not exists: '${images_root_dir}'"
+    exit 1
+fi
+
+# Print input parameters
+echo "Images root directory: ${images_root_dir}"
+
+# Find images to upload
+echo "Searching for images to upload..."
+images=$(find "${images_root_dir}" -type f -name *.png)
+images_num=$(wc -l <<< "${images}")
+
+echo "Images found: ${images_num}"
+
+# Cleanup output files
+echo "Cleaning up output files: ${output_success}, ${output_failed}"
+echo -n "" > "${output_success}"
+echo -n "" > "${output_failed}"
+
+# Upload images one by one
+for image in $images; do
+    echo "Uploading image: ${image} Size: $(fsize_print "${image}")"
+
+    # Upload image using curl
+    curl_output=$(curl_upload_file "${image}" "${endpoint}")
+    curl_ret=$?
+
+    # Parse curl response headers
+    http_ret=$(http_codes_list "${curl_output}" | tail -n 1)
+
+    # Check result
+    if [ ${curl_ret} -eq 0 ]; then
+        url=$(image_url "${curl_output}")
+        echo "HTTP: ${http_ret} Image URL: ${url}"
+        echo "${image}: ${url}" >> "${output_success}"
+    else
+        echo "Upload failed: HTTP: ${http_ret} Image: ${image}"
+        echo "${image}: HTTP ${http_ret}" >> "${output_failed}"
+    fi
+done
